95f1da7a7b9606a021ab66ba1b48f55bbbdd7380
mboky <m.boky@student.tudelft.nl>
2024-02-19 14:37:16 +0100

    Made the measurements operators accesable

--- netket_fidelity/operator/__init__.py
+++ netket_fidelity/operator/__init__.py
@@ -1,9 +1,7 @@
-from .singlequbit_gates import Rx, Ry, Hadamard
+from netket.utils import _hide_submodules
+
+from .singlequbit_gates import Rx, Ry, Hadamard, Measure_0, Measure_1
 
 # from .ising import Ising
-from netket.operator import IsingJax as Ising
-
-
-from netket.utils import _hide_submodules
 
 _hide_submodules(__name__, hide_folder=["singlequbit_gates"])
